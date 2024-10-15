c927fefc2e0a35171d025f704b87922055fab1d0
HAEDAL-IRLAB <jyh4282002@naver.com>
2024-10-15 14:58:53 +0900

    Initial commit

---
+++ src/main/java/com/sparta/jpa_crud_project/JpaCrudProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.jpa_crud_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaCrudProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/jpa_crud_project/JpaCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.jpa_crud_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
